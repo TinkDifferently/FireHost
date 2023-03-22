684cb5d95e57352ac7ce71e78caa54ad3dad7dda
a@a.a <a@a.a>
2023-03-22 09:17:31 +0300

    init

--- src/main/java/ru/rosbank/connection/Connector.java
+++ src/main/java/ru/rosbank/connection/Connector.java
@@ -2,20 +2,28 @@ package ru.rosbank.connection;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.SneakyThrows;
+import org.jetbrains.annotations.NotNull;
+import ru.chrome.ext.api.Session;
 import ru.chrome.ext.api.session.SessionData;
 import ru.chrome.ext.toApp.FireServer;
 
 import java.nio.file.Path;
+import java.util.HashMap;
+import java.util.Map;
 
 public class Connector {
-    private static final Connector instance = new Connector();
+
+    private static final Map<String, Connector> connectors = new HashMap<>();
+
+    private final Session session;
     private final FireServer server;
 
-    public static Connector getInstance() {
-        return instance;
+    public static Connector getInstance(@NotNull String session) {
+        return connectors.computeIfAbsent(session,Connector::new);
     }
 
-    public Connector() {
+    private Connector(String session) {
+        this.session = Session.create(session);
         server = new FireServer("C:\\Users\\tinkd\\AppData\\Roaming\\Inist\\FireWyrmNativeMessageHost.proxy.exe",
                 "polkfpcimpfhcflopocaobbbnphilcbi");
         server.run();
@@ -34,6 +42,6 @@ public class Connector {
     }
 
     public void sendMessage(String initialMessage, Path path) {
-       server.mountObserver(initialMessage,path);
+        server.mountObserver(initialMessage, path, session);
     }
 }
